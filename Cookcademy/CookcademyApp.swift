c871ca6031575c104018dfde0c0c7b9f6d647a40
Anastasiya Kiptsevich <anastasiyakiptsevich@Anastasiyas-MacBook-Pro.local>
2024-03-18 19:00:00 +0100

    Initial commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Anastasiya Kiptsevich on 18.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
