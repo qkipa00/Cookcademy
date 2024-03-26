68a4b7c59662e0c15ff33587e9ac885c8b31daf8
Anastasija Kipcevic <akiptsevich@gmail.com>
2024-03-26 17:03:32 +0100

    Building a form to edit a direction

---
+++ Cookcademy/Views/ModifyRecipes/ModifyDirectionView.swift
@@ -0,0 +1,45 @@
+//
+//  ModifyDirectionView.swift
+//  Cookcademy
+//
+//  Created by Anastasiya Kiptsevich on 26.03.2024.
+//
+
+import SwiftUI
+
+struct ModifyDirectionView: View {
+    @Binding var direction: Direction
+    let createAction: (Direction) -> Void
+    
+    private let listBackgroundColor = AppColor.background
+    private let listTextColor = AppColor.foreground
+    
+    @Environment(\.presentationMode) private var mode
+    
+    var body: some View {
+        Form {
+            TextField("Direction Description", text: $direction.description)
+                .listRowBackground(listBackgroundColor)
+            Toggle("Optional", isOn: $direction.isOptional)
+                .listRowBackground(listBackgroundColor)
+            HStack {
+                Spacer()
+                Button("Save") {
+                    createAction(direction)
+                    mode.wrappedValue.dismiss()
+                }
+                Spacer()
+            }.listRowBackground(listBackgroundColor)
+        }
+        .foregroundColor(listTextColor)
+    }
+}
+
+struct ModifyDirectionView_Previews: PreviewProvider {
+    @State static var emptyDirection = Direction(description: "", isOptional: false)
+    static var previews: some View {
+        NavigationView {
+            ModifyDirectionView(direction: $emptyDirection) { _ in return}
+        }
+    }
+}
