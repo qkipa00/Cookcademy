4aee538c22240ad94ae426e53ca5d249b9de6f70
Anastasija Kipcevic <akiptsevich@gmail.com>
2024-03-27 14:44:39 +0100

    Edit recipes changes

--- Cookcademy/Views/ExploreRecipes/RecipeDetailView.swift
+++ Cookcademy/Views/ExploreRecipes/RecipeDetailView.swift
@@ -8,10 +8,12 @@
 import SwiftUI
 
 struct RecipeDetailView: View {
-    let recipe: Recipe
+    @Binding var recipe: Recipe
     
     private let listBackgroundColor = AppColor.background
     private let listTextColor = AppColor.foreground
+    
+    @State private var isPresenting = false
     
     var body: some View {
         VStack {
@@ -51,6 +53,28 @@ struct RecipeDetailView: View {
             }
         }
         .navigationTitle(recipe.mainInformation.name)
+        .toolbar {
+            ToolbarItem {
+                HStack {
+                    Button("Edit") {
+                        isPresenting = true
+                    }
+                }
+            }
+        }
+        .sheet(isPresented: $isPresenting) {
+            NavigationView {
+                ModifyRecipeView(recipe: $recipe)
+                    .toolbar {
+                        ToolbarItem(placement: .confirmationAction) {
+                            Button("Save") {
+                                isPresenting = false
+                            }
+                        }
+                    }
+                    .navigationTitle("Edit Recipe")
+            }
+        }
     }
 }
 
@@ -58,7 +82,7 @@ struct RecipeDetailView_Previews: PreviewProvider {
   @State static var recipe = Recipe.testRecipes[0]
   static var previews: some View {
     NavigationView {
-      RecipeDetailView(recipe: recipe)
+      RecipeDetailView(recipe: $recipe)
     }
   }
 }
